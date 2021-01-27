d9fa69efde11d098fd1f8b71ba142ac2aba8dab9
fei yang <feiyang@192-168-1-151.tpgi.com.au>
2021-01-27 16:47:39 +1100

    the algorithm

--- tennis.py
+++ tennis.py
@@ -3,8 +3,17 @@ Lines = file1.readlines()
 Lines.append("\nM")
 
 pointArrays = [] # [[0,1,1],[1,1,0,0],..]
-gameArrays = [] # [[23, 17], [1, 1], ..]
+gameArrays = [] # [[2, 1], [2, 1], ..]
 playerArrays = [] # [[A,C], [A,B], ...]
+
+# Score Match 01: Player playerArrays[0] gameArrays[0][0] > [0][1]? playerArrays[1]
+# gameArrays[0][0] sets to [0][1]
+
+# Games Player A
+# wins = 0 , lose = 0
+# for i,array in playerArrays:
+# 	index= array.find(A), if index = 0, wins += pointArrays[i].count(0)  loses += len(pointArrays[i]) - pointArrays[i].count(0)
+# pring win, lose 
 
 def calculateGames(pointArray):
 	print("hey")
